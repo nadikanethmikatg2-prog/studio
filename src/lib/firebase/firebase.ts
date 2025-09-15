00b0e92ab5d765afbf200a4fb4780971816ce3a3
Nadika Nethmika <nadikanethmikatg1@gmail.com>
2025-09-15 15:22:28 +0000

    FirebaseError: Failed to get document because the client is offline.

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -1,6 +1,12 @@
 import { initializeApp, getApps, getApp, type FirebaseApp } from "firebase/app";
 import { getAuth, connectAuthEmulator, type Auth } from "firebase/auth";
-import { getFirestore, connectFirestoreEmulator, type Firestore } from "firebase/firestore";
+import { 
+  getFirestore, 
+  connectFirestoreEmulator, 
+  type Firestore,
+  initializeFirestore,
+  enableIndexedDbPersistence 
+} from "firebase/firestore";
 
 // IMPORTANT: REPLACE THIS WITH YOUR ACTUAL FIREBASE CONFIG
 const firebaseConfig = {
@@ -12,16 +18,25 @@ const firebaseConfig = {
   appId: "1:680938869975:web:fe28e2865bf8ac09394c92"
 };
 
-function initializeFirebaseApp() {
-  if (getApps().length > 0) {
-    return getApp();
-  }
-  return initializeApp(firebaseConfig);
+const app: FirebaseApp = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+const auth: Auth = getAuth(app);
+const db: Firestore = initializeFirestore(app, {});
+
+if (typeof window !== 'undefined') {
+  enableIndexedDbPersistence(db)
+  .catch((err) => {
+    if (err.code == 'failed-precondition') {
+      console.warn(
+        'Multiple tabs open, persistence can only be enabled in one tab at a time.'
+      );
+    } else if (err.code == 'unimplemented') {
+      console.warn(
+        'The current browser does not support all of the features required to enable persistence.'
+      );
+    }
+  });
 }
 
-const app: FirebaseApp = initializeFirebaseApp();
-const auth: Auth = getAuth(app);
-const db: Firestore = getFirestore(app);
 
 if (process.env.NODE_ENV === "development" && process.env.NEXT_PUBLIC_USE_FIREBASE_EMULATOR === 'true') {
   try {
