03f909bb8a5ee45c1f82e2d04a6787f24044118b
Nadika Nethmika <nadikanethmikatg1@gmail.com>
2025-09-17 15:56:25 +0000

    In order to handle the work load can we use multiple api keys

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,10 +1,15 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
+function getApiKey(): string {
+  const anyscaleKeys = (process.env.GEMINI_API_KEYS || '').split(',');
+  return anyscaleKeys[Math.floor(Math.random() * anyscaleKeys.length)];
+}
+
 export const ai = genkit({
   plugins: [
     googleAI({
-      apiKey: process.env.GEMINI_API_KEY,
+      apiKey: getApiKey,
     }),
   ],
   model: 'googleai/gemini-2.5-flash',
