36dc54fafabbc210cf2a1dee6ed7066749be370e
Nadika Nethmika <nadikanethmikatg1@gmail.com>
2025-10-07 05:05:22 +0000

    Runtime Error NotAllowedError: Failed to execute 'writeText' on 'Clipboa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -344,7 +344,7 @@ export default function Home() {
             <SubjectDetailsCard
               subjects={subjects}
               onUpdate={handleUpdate}
-              onLogHours={onLogHours}
+              onLogHours={handleLogHours}
             />
           </div>
 
