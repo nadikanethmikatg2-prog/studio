651215f9f79e460e45afae9c99446bebb1078ce5
Nadika Nethmika <nadikanethmikatg1@gmail.com>
2025-10-07 05:44:12 +0000

    You are attempting to export "metadata" from a component marked with "us

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
-import { cn } from '@/lib/utils';
 import { Providers } from './providers';
 
 export const metadata: Metadata = {
@@ -25,9 +24,7 @@ export default function RootLayout({
         />
       </head>
       <body
-        className={cn(
-          'min-h-screen bg-background font-sans antialiased flex flex-col'
-        )}
+        className='min-h-screen bg-background font-sans antialiased flex flex-col'
       >
         <Providers>
           {children}
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,6 +2,7 @@
 
 import { AuthProvider } from '@/hooks/use-auth';
 import { LanguageProvider } from '@/hooks/use-language';
+import { cn } from '@/lib/utils';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
